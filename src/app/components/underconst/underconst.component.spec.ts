0bdf1acc1489c64dc775bb1085df60c49522289d
ging <charbel.alhelou@ensa-paris.fr>
2021-08-09 16:48:40 +0200

    ...

--- src/app/components/underconst/underconst.component.spec.ts
+++ src/app/components/underconst/underconst.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { UnderconstComponent } from './underconst.component';
 
@@ -6,7 +6,7 @@ describe('UnderconstComponent', () => {
   let component: UnderconstComponent;
   let fixture: ComponentFixture<UnderconstComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ UnderconstComponent ]
     })
